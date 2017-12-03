816b6f61b19f7ffbc54d124c5c02fe15ac8a8d67
Brian Sunter <bsunter@gmail.com>
2017-12-03 15:55:44 -0800

    Transduce

--- src/advent_of_code/2017/day_1.clj
+++ src/advent_of_code/2017/day_1.clj
@@ -1,13 +1,17 @@
 (ns advent-of-code.2017.day-1)
 
+(def group-next-digits-xforms
+  (comp
+   (partition-by identity)
+   (filter #(<= 2 (count %)))
+   (map #(drop 1 %))))
+
 (defn sum-next-digits
   [digits]
-  (let [digit-list (into [] (map #(Character/digit % 10) digits))
-        circular-digit-list  (conj digit-list (first digit-list))]
-    (->>
-     circular-digit-list
-     (partition-by identity)
-     (filter #(<= 2 (count %)))
-     (map #(drop 1 %))
-     (map (partial reduce +))
-     (reduce +))))
+  (let [circular-digits (str digits (first digits))]
+    (transduce (comp
+               (map #(Character/digit % 10))
+               group-next-digits-xforms
+               (map (partial reduce +)))
+              +
+              circular-digits)))
