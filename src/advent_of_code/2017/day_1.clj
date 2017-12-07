f7e1ca952a1823a5ec54cd3a30afd1ab23328845
Brian Sunter <bsunter@gmail.com>
2017-12-06 20:34:43 -0800

    Solve Day 1

--- src/advent_of_code/2017/day_1.clj
+++ src/advent_of_code/2017/day_1.clj
@@ -1,17 +1,56 @@
-(ns advent-of-code.2017.day-1)
+(ns advent-of-code.2017.day-1
+  (:require [clojure.spec.alpha :as s]
+            [clojure.spec.gen.alpha :as gen]
+            [clojure.java.io :as io]))
 
-(def group-next-digits-xforms
+(def group-sequential-digits-xforms
   (comp
    (partition-by identity)
-   (filter #(<= 2 (count %)))
+   (filter #(<= 2 (count %)))))
+
+(def group-leading-digits-xforms
+  (comp
+   group-sequential-digits-xforms
    (map #(drop 1 %))))
 
-(defn sum-next-digits
+(def char->number-xforms (map #(Character/digit % 10)))
+
+(s/def ::digit-string
+  (s/with-gen #(number? (read-string %))
+    #(gen/fmap str (s/gen pos-int?))))
+
+(defn sum-number-groups-reduce-fn
+  ([]  0)
+  ([i] i)
+  ([r i] (+ r (reduce + i))))
+
+(def sum-leading-digits-xforms
+  (comp
+   char->number-xforms
+   group-leading-digits-xforms))
+
+(s/fdef sum-leading-digits
+        :args (s/cat :digits ::digit-string)
+        :ret #(<= 0 %))
+
+(defn sum-leading-digits
   [digits]
   (let [circular-digits (str digits (first digits))]
-    (transduce (comp
-               (map #(Character/digit % 10))
-               group-next-digits-xforms
-               (map (partial reduce +)))
-              +
-              circular-digits)))
+    (transduce group-leading-digits-xforms
+               sum-number-groups-reduce-fn
+               circular-digits)))
+
+(defn read-puzzle-input
+  [filename]
+  (some->
+   (io/resource "2017/day_1_input.txt")
+   io/file
+   slurp
+   clojure.string/trim))
+
+(defn solve-puzzle
+  []
+  (->>
+   (read-puzzle-input "2017/day_1_input.txt")
+   (s/assert ::digit-string)
+   (sum-leading-digits)))
