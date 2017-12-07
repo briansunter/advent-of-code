58f97c34da63fcf484a03756c86a680ae8a39fb3
Brian Sunter <bsunter@gmail.com>
2017-12-06 20:34:43 -0800

    Solve Day 1

--- src/advent_of_code/2017/day_1.clj
+++ src/advent_of_code/2017/day_1.clj
@@ -1,4 +1,7 @@
-(ns advent-of-code.2017.day-1)
+(ns advent-of-code.2017.day-1
+  (:require [clojure.spec.alpha :as s]
+            [clojure.spec.gen.alpha :as gen]
+            [clojure.java.io :as io]))
 
 (def group-next-digits-xforms
   (comp
@@ -6,12 +9,32 @@
    (filter #(<= 2 (count %)))
    (map #(drop 1 %))))
 
+(s/def ::digit-string
+  (s/with-gen #(number? (read-string %))
+    #(gen/fmap str (s/gen pos-int?))))
+
+(s/fdef sum-next-digits
+        :args (s/cat :digits ::digit-string)
+        :ret #(<= 0 %))
+
 (defn sum-next-digits
   [digits]
   (let [circular-digits (str digits (first digits))]
     (transduce (comp
-               (map #(Character/digit % 10))
-               group-next-digits-xforms
-               (map (partial reduce +)))
-              +
-              circular-digits)))
+                (map #(Character/digit % 10))
+                group-next-digits-xforms
+                (map (partial reduce +)))
+               +
+               circular-digits)))
+
+(defn read-puzzle-input
+  [filename]
+  (some->>
+   (io/resource "2017/day_1_input.txt")
+   (io/file)
+   slurp
+   clojure.string/trim))
+
+(defn solve-puzzle
+  []
+  (sum-next-digits (read-puzzle-input "2017/day_1_input.txt")))
--- test/advent_of_code/2017/day_1_test.clj
+++ test/advent_of_code/2017/day_1_test.clj
@@ -1,10 +1,22 @@
 (ns advent-of-code.2017.day-1-test
   (:require [advent-of-code.2017.day-1 :as sut]
+            [clojure.test.check :as tc]
+            [clojure.spec.test.alpha :as st]
+            [clojure.pprint :as pprint]
             [clojure.test :refer [deftest testing is]]))
+
+;; Utility functions to intergrate clojure.spec.test/check with clojure.test
+(defn summarize-results' [spec-check]
+  (map (comp #(pprint/write % :stream nil) st/abbrev-result) spec-check))
+
+(defn check' [spec-check]
+  (is (nil? (-> spec-check first :failure)) (summarize-results' spec-check)))
 
 (deftest day-1
   (testing "Sample Input"
     (is (= 3 (sut/sum-next-digits "1122")))
     (is (= 4 (sut/sum-next-digits "1111")))
     (is (= 0 (sut/sum-next-digits "1234")))
-    (is (= 9 (sut/sum-next-digits "91212129")))))
+    (is (= 9 (sut/sum-next-digits "91212129"))))
+  (testing "Generated Input"
+    (check' (st/check `sut/sum-next-digits))))
